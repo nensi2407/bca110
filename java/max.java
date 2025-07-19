3f397c1b1189d656a79825b4ad348c854f9ba027
Dhruvi2629 <dhruvigalani26@gmail.com>
2025-07-19 13:58:40 +0530

    Initial commit

---
+++ java/max.java
@@ -0,0 +1,8 @@
+public class P2{
+public static void main(string[] args){
+	int a=10,b=25,c=15;
+	int max=((a>b)? a:c):((b>c)? b:c);
+	
+	System.out.println("Maximum of"+a+", "+b+"and"+c+"is:"+max);
+}
+}
---
+++ java/pro1.java
@@ -0,0 +1,25 @@
+public class primenumbers{
+public static void main(string[] args){
+int n=10;
+int count=0;
+int num=2;
+
+System.out.println("first"+n+"prime numbers are:");
+while(count<n){
+if(isprime(num)){
+System.out.println(num+" ");
+count++;
+}
+num++;
+}
+}
+public static boolean isprime(int num){
+if(num<2)
+return false;
+for(int i=2; i<=num/2;i++){
+if (num % i==0)
+return false;
+}
+return true;
+}
+}
